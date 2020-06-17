925207226fa84cb2c7c5ccc6c6b02d8a557de2bc
Каролина Чурейно <kchureyno@outlook.com>
2020-06-17 13:03:18 +0300

    first look

--- rels/Form1.Designer.cs
+++ rels/Form1.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(60, 59);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(624, 333);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- rels/Form1.cs
+++ rels/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,64 @@ namespace rels
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private string next = "Alexandra Feodorovna (Alix of Hesse)";
+
+        private Stack<string> nexts = new Stack<string>();
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            nexts.Push("Alexandra Feodorovna (Alix of Hesse)");
+            HtmlWeb web = new HtmlWeb();
+            while (nexts.Count > 0)
+            {
+                var page = await web.LoadFromWebAsync("https://en.wikipedia.org/wiki/" + nexts.Pop());
+                var rows = page.DocumentNode.SelectNodes("//table[@class='infobox vcard']/tbody/tr");
+                rows.Where(r => Filter(r, "Father")).Select(r => Map(r))
+                    .Where(r => r != null).ToList()
+                    .ForEach(t =>
+                    {
+                        richTextBox1.AppendText(t.Attributes["title"].Value + string.Format("{0}", nexts.Count) + "\r\n");
+                        if (!string.IsNullOrEmpty(t.Attributes["title"].Value))
+                        {
+                            nexts.Push(t.Attributes["title"].Value);
+                        }
+                    });
+                rows.Where(r => Filter(r, "Mother")).Select(r => Map(r))
+                    .Where(r => r != null).ToList()
+                    .ForEach(t =>
+                    {
+                        richTextBox1.AppendText(t.Attributes["title"].Value + string.Format("{0}", nexts.Count) + "\r\n");
+                        if (!string.IsNullOrEmpty(t.Attributes["title"].Value))
+                        {
+                            nexts.Push(t.Attributes["title"].Value);
+                        }
+                    });
+            }
+        }
+
+        private HtmlNode Map(HtmlNode node)
+        {
+            var children = node.SelectNodes("td/a[@href]");
+            if (children != null)
+            {
+                return children.First();
+            }
+            return null;
+        }
+
+        private bool Filter(HtmlNode node, string text)
+        {
+            var ths = node.SelectNodes("th[@scope='row']");
+            if (ths != null)
+            {
+                if (ths.First().InnerText.Equals(text))
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
