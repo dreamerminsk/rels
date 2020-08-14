ffa7de9b5832131395851880324ab1b35d7fea47
Каролина Чурейно <kchureyno@outlook.com>
2020-08-14 18:53:25 +0300

    Instances

---
+++ rels/Model/Instance.cs
@@ -0,0 +1,24 @@
+﻿using LinqToDB.Common;
+using LinqToDB.Mapping;
+
+namespace rels.Model
+{
+    [Table(Name = "Instances")]
+    public class Instance
+    {
+        private string _name = "en: ???????";
+
+        [PrimaryKey, Identity]
+        public int ID { get; set; }
+
+        [Column(Name = "WikiDataID")]
+        public string WikiDataID { get; set; }
+
+        [Column(Name = "Name"), NotNull]
+        public string Name { get { return _name; } set { if (!value.IsNullOrEmpty()) _name = value; } }
+
+        [Column(Name = "RusName")]
+        public string RusName { get; set; } = "ru: ???????";
+
+    }
+}
---
+++ rels/Model/Instances.cs
@@ -0,0 +1,42 @@
+﻿using LinqToDB;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rels.Model
+{
+    public class Instances
+    {
+        public static bool IsExists(string wikiDataId)
+        {
+            using (var db = new RelsDB())
+            {
+                var instances = db.GetTable<Instance>();
+                return instances.Any(p => p.WikiDataID.Equals(wikiDataId));
+            }
+        }
+
+        public static Instance GetByWikiDataId(string wikiDataId)
+        {
+            using (var db = new RelsDB())
+            {
+                var instances = db.GetTable<Instance>();
+                return instances.Where(p => p.WikiDataID.Equals(wikiDataId)).FirstOrDefault();
+            }
+        }
+
+        public static async Task<int> InsertAsync(Instance c)
+        {
+            using (var db = new RelsDB())
+            {
+                try
+                {
+                    return await db.InsertAsync(c);
+                }
+                catch
+                {
+                    return -1;
+                }
+            }
+        }
+    }
+}
