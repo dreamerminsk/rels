e82195245c4f56356ee199da6ca1d951b8abd7e6
Каролина Чурейно <kchureyno@outlook.com>
2020-09-01 12:41:15 +0300

    remove tut.by parser

--- news/Form1.cs
+++ news/Form1.cs
@@ -22,28 +22,6 @@ namespace news
 
         private async void Form1_Load(object sender, EventArgs e)
         {
-            await timeConstraint;
-            var page = await htmlWeb.LoadFromWebAsync("https://www.tut.by/");
-            var newsEntries = page.DocumentNode.SelectNodes("//div[contains(@class, 'news-entry')]");
-            newsEntries.ToList().ForEach(newsEntry =>
-            {
-                richTextBox1.AppendText("ID: " + (newsEntry.Attributes["data-id"]?.Value ?? "-1") + "\r\n");
-                var spanTitle = newsEntry.SelectSingleNode("a/span/span[contains(@class, '_title')]");
-                if (spanTitle != null)
-                {
-                    richTextBox1.AppendText("TITLE: " + HttpUtility.HtmlDecode(spanTitle.InnerText) + "\r\n");
-                }
-                richTextBox1.AppendText("PUBLISHED: " +
-                    UnixTimeStampToDateTime(double.Parse(newsEntry.Attributes["data-tm"]?.Value ?? "0")).ToString() + "\r\n");
-                richTextBox1.AppendText("UPDATED: " +
-                    UnixTimeStampToDateTime(double.Parse(newsEntry.Attributes["data-update-tm"]?.Value ?? "0")).ToString() + "\r\n");
-                var spanComments = newsEntry.SelectSingleNode("a/span[@class='entry-cnt']/span[@class='entry-meta']/span[@class='entry-count']");
-                if (spanComments != null)
-                {
-                    richTextBox1.AppendText("COMMENTS: " + spanComments.InnerText + "\r\n");
-                }
-                richTextBox1.AppendText(newsEntry.InnerHtml + "\r\n" + new string('-', 100) + "\r\n");
-            });
         }
 
         public DateTime UnixTimeStampToDateTime(double unixTimeStamp)
